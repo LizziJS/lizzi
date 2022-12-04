8bb00a43f0802f9eb0474f9685c1270b71c0f776
Stanislav Shishankin <57766849+myrpg-fun@users.noreply.github.com>
2022-12-04 22:56:39 +0400

    change config

--- examples/live/webpack.config.js
+++ examples/live/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "../../docs/live"),
-    publicPath: "/",
+    publicPath: "./",
   },
   resolve: {
     modules: ["node_modules"],
