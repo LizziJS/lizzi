c33930f09a52a85d690d8d4f13e778040e1dccc8
StasCosmos <stas@cosmos.video>
2023-05-01 22:15:45 +0400

    add isolate

--- packages/jsx-runtime/src/index.ts
+++ packages/jsx-runtime/src/index.ts
@@ -8,6 +8,7 @@ import {
 } from "@lizzi/template";
 import { isSvgTag } from "./SvgTags";
 import { isNodeConstructor, zzNode } from "@lizzi/node";
+import { zzGetReactiveObserver } from "@lizzi/core";
 
 export const jsx = <T extends zzNode>(
   type: string | T,
@@ -27,23 +28,33 @@ export const jsxs = <T extends zzNode>(
     [k: string]: any;
   }
 ) => {
-  if (typeof type === "string") {
-    if (isSvgTag.has(type as any) || props.svg) {
-      return new SvgElementView(type as any, props);
-    }
+  let result;
 
-    return new HtmlElementView(type as any, props);
-  } else if (typeof type === "function") {
-    if (type[isNodeConstructor]) {
-      return new (type as new (props: object) => T)(props);
-    }
+  zzGetReactiveObserver.runIsolated(() => {
+    if (typeof type === "string") {
+      if (isSvgTag.has(type as any) || props.svg) {
+        result = new SvgElementView(type as any, props);
+        return;
+      }
 
-    return (type as (props: object) => T)(props);
-  } else {
-    throw new Error(
-      "JSX constructor should be string, HTML tag or zzNode constructor"
-    );
-  }
+      result = new HtmlElementView(type as any, props);
+      return;
+    } else if (typeof type === "function") {
+      if (type[isNodeConstructor]) {
+        result = new (type as new (props: object) => T)(props);
+        return;
+      }
+
+      result = (type as (props: object) => T)(props);
+      return;
+    } else {
+      throw new Error(
+        "JSX constructor should be string, HTML tag or zzNode constructor"
+      );
+    }
+  });
+
+  return result;
 };
 
 export const Fragment = zzHtmlComponent;
